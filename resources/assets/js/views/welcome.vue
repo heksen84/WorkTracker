09a2100663e0262dd04da20ee3a694f4edd3b00f
Ilya Bobkov <Ilya.bobkov@erg.kz>
2018-09-25 14:17:52 +0600

    up+

--- resources/assets/js/views/welcome.vue
+++ resources/assets/js/views/welcome.vue
@@ -14,21 +14,21 @@
   </b-navbar>
 </div>
 
-    <b-row class="text-center" style="margin-top:30px">
-    	<b-col cols="12">
-				<h1 id="title-text">ОтБаке</h1>
-				<div style="font-size:154%;margin-top:-14px;margin-bottom:17px;letter-spacing:5px;color:rgb(90,90,90);font-weight:550">Сайт на котором есть работа</div>
-				<a href="find-work"><b-button class="mb-1 mr-sm-0 mb-sm-0">Найти работу</b-button></a>
-				<b-button href="offer-a-job" variant="success" @click="test" class="mb-0 mr-sm-0 mb-sm-0">Разместить вакансию</b-button>
-    	</b-col>
-    </b-row>
+<b-row class="text-center" style="margin-top:30px">
+ <b-col cols="12">
+	<h1 id="title-text">ОтБаке</h1>
+	<div style="font-size:154%;margin-top:-14px;margin-bottom:17px;letter-spacing:5px;color:rgb(90,90,90);font-weight:550">Сайт на котором есть работа</div>
+	<a href="find-work"><b-button class="mb-1 mr-sm-0 mb-sm-0">Найти работу</b-button></a>
+	<b-button href="offer-a-job" variant="success" @click="test" class="mb-0 mr-sm-0 mb-sm-0">Разместить вакансию</b-button>
+ </b-col>
+</b-row>
 
 <b-row style="margin-top:5%">
 <b-col>	
 <b-form inline style="margin:auto;width:500px">
 <b-form-group label="<b>Поиск работы по специальности:</b>">
-		<b-form-input type="text" placeholder="Введите название специальности" class="mb-0 mr-sm-0 mb-sm-0" style="width:420px" v-model="search"></b-form-input>
-		<b-button variant="primary" style="margin-left:5px" @click="searchData">Найти</b-button>
+	<b-form-input type="text" placeholder="Введите название специальности" class="mb-0 mr-sm-0 mb-sm-0" style="width:420px" v-model="search"></b-form-input>
+	<b-button variant="primary" style="margin-left:5px" @click="searchData">Найти</b-button>
 </b-form-group>
 </b-form>
 </b-col>
